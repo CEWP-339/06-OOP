eab35b5900e1723c9f7542399cedcb48262219f3
Kritika Sharma <kritikasharma@Kritikas-MacBook-Pro.local>
2022-08-08 20:59:38 -0400

    private method

--- oop-example.js
+++ oop-example.js
@@ -3,22 +3,18 @@ class Item {
     #name = "default name";
     #description = "default description";
 
-    constructor () {
-       console.log("In the constructor!");
-    }
-
-    sayHello (name) {
+    #sayHello (name) {
          console.log(`Hello, ${name}`);
     }
+
+
 
 }
 
 item1 = new Item();
-item1.sayHello("Monica");
-console.log(item1.#name);
+item1.#sayHello("Monica");
 console.log("item1: ", item1);
 
 item2 = new Item();
-item2.sayHello("Chandler");
-console.log(item2.#description);
+item2.#sayHello("Chandler");
 console.log("item2: ", item2);
