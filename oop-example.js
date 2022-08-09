d6173c9035dce027953533d41065a04bca099abd
Kritika Sharma <kritikasharma@Kritikas-MacBook-Pro.local>
2022-08-08 21:10:30 -0400

    getters and setters

--- oop-example.js
+++ oop-example.js
@@ -7,14 +7,34 @@ class Item {
          console.log(`Hello, ${name}`);
     }
 
+    get getName () {
+        return this.#name;
+    }
 
+    set setName (name) {
+        this.#name = name;
+    }
+
+    get getDescription () {
+        return this.#description;
+    }
+
+    set setDescription (description) {
+        this.#description = description;
+    }
 
 }
 
 item1 = new Item();
-item1.#sayHello("Monica");
+item1.setName = "Chandler";
+console.log(item1.getName);
+item1.setDescription = "I am Item 1!";
+console.log(item1.getDescription);
 console.log("item1: ", item1);
 
 item2 = new Item();
-item2.#sayHello("Chandler");
+item2.setName = "Monica";
+console.log(item2.getName);
+item2.setDescription = "I am Item 2!";
+console.log(item2.getDescription);
 console.log("item2: ", item2);
