928738cfbe4f63031880153df521c7a422444c1c
Kritika Sharma <kritikasharma@Kritikas-MacBook-Pro.local>
2022-08-08 20:49:22 -0400

    empty constructor

--- oop-example.js
+++ oop-example.js
@@ -3,9 +3,8 @@ class Item {
     name;
     description;
 
-    constructor (name, description) {
-        this.name = name;
-        this.description = description;
+    constructor () {
+       console.log("In the constructor!");
     }
 
     sayHello (name) {
@@ -14,10 +13,10 @@ class Item {
 
 }
 
-item1 = new Item("Jelo", "I am Item 1!");
+item1 = new Item();
 item1.sayHello("Monica");
 console.log("item1: ", item1);
 
-item2 = new Item("ks", "I am Item 2!");
+item2 = new Item();
 item2.sayHello("Chandler");
 console.log("item2: ", item2);
