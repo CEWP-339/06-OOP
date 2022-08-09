b788921d1c7a1e6efeee45dba749acd45e5df516
Kritika Sharma <kritikasharma@Kritikas-MacBook-Pro.local>
2022-08-08 21:25:33 -0400

    static keyword

--- oop-example.js
+++ oop-example.js
@@ -2,6 +2,7 @@ class Item {
 
     #name = "default name";
     #description = "default description";
+    static type = "Book";
 
     #sayHello (name) {
          console.log(`Hello, ${name}`);
@@ -26,15 +27,17 @@ class Item {
 }
 
 item1 = new Item();
-item1.setName = "Chandler";
-console.log(item1.getName);
-item1.setDescription = "I am Item 1!";
-console.log(item1.getDescription);
+// item1.setName = "Chandler";
+// console.log(item1.getName);
+// item1.setDescription = "I am Item 1!";
+// console.log(item1.getDescription);
+console.log("static property for Item 1: ", Item.type);
 console.log("item1: ", item1);
 
 item2 = new Item();
-item2.setName = "Monica";
-console.log(item2.getName);
-item2.setDescription = "I am Item 2!";
-console.log(item2.getDescription);
+// item2.setName = "Monica";
+// console.log(item2.getName);
+// item2.setDescription = "I am Item 2!";
+// console.log(item2.getDescription);
+console.log("static property for Item 2: ", Item.type);
 console.log("item2: ", item2);
